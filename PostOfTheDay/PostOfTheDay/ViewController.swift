309ca5503d8a8d3551cb3af5a0e46e66c8a0a663
Aidan Hobler <ajh343@cornell.edu>
2019-11-18 20:41:47 -0500

    Init

---
+++ PostOfTheDay/PostOfTheDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostOfTheDay
+//
+//  Created by Aidan Hobler on 11/18/19.
+//  Copyright © 2019 Aidan Hobler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
